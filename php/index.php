55bb3fbe0f68bbd089e97d4b5374b01ef704d3d6
David-Lannion <62551888+David-Lannion@users.noreply.github.com>
2023-04-18 15:34:31 +0200

    +

--- php/index.php
+++ php/index.php
@@ -4,11 +4,31 @@ $user = 'myuser';
 $password = 'monpassword';
 $db = 'iai_php';
 
+// Connect using mysqli
 $conn = new mysqli($host,$user,$password,$db);
 if(!$conn) {echo "Erreur de connexion à MySQL<br />";}
 else{
         echo "Connexion à MySQL ok<br />";
         mysqli_close($conn);
+}
+// Connect using PDO mysql
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$db", $user, $password);
+    echo "Connexion à MySQL PDO ok<br />";
+}
+catch (Exception $e){
+    echo "Erreur de connexion à MySQL PDO<br />";
+}
+// Connect using PDO pgsql
+try {
+    $conn = new PDO("pgsql:host=postgres;dbname=$db", $user, $password);
+    echo "Connexion à PDO PGSQL ok<br />";
+}
+catch (Exception $e){
+    echo "<pre>";
+    var_dump($e);
+    echo "</pre>";
+    echo "Erreur de connexion à PGSQL PDO<br />";
 }
 phpinfo();
 ?>
